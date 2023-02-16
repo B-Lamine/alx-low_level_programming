0bfc883af3142f5ce44ede2d8280e9e15fc095d6
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-02-16 15:48:54 -0800

    displaying text using printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-
 /*
- *print "Programming is like building a multilingual puzzle
- *to the st IO
+ *print "Programming is like building a multilingual puzzle to the std IO
  */
 int main(void)
 {
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*
+ *displays a sentence using printf
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
