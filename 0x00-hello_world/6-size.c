fcd6285ceff95e103565ae3189c44907f2e86236
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-02-16 16:02:07 -0800

    print sizes of variable types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <std.io>
+/*
+ *
+ */
+int main(void)
+{
+	int i;
+	char c;
+	float f;
+	printf("Size of a char: %lu byte(s)\n",sizeof(c));
+	printf("Size of an int: %lu byte(s)\n",sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n",(long)sizeof(i));
+	printf("Size of a long long int: %lu byte(s)\n",(long long)sizeof(i));
+	printf("Size of a float: %lu byte(s)\n",sizeof(f));
+	return (0);
+}
