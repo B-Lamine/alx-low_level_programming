5128585cc2c6b721f7bf4f7c7bcf7decf9d5a43d
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-02-17 15:43:18 -0800

    print single digits in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits in base 16
+ *
+ * Return: 0 is code successful
+ */
+int main(void)
+{
+	int d = 0x30;
+
+	while (d <= 0x46)
+	{
+		if (d <= 0x39 | d >= 0x41)
+			putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
