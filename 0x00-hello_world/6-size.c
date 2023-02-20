5a4ffac869830f880c8daf198091e2460cf2972f
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-02-20 11:31:18 -0800

    last correction of 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
-/*
- *print sizes of different types of variables
+
+/**
+ * main - print sizes of different types of variables
+ *
+ * Return: 0 for successful code
  */
 int main(void)
 {
 	int i;
+	long int j;
+	long long int k;
 	char c;
 	float f;
-	printf("Size of a char: %i byte(s)\n",sizeof(c));
-	printf("Size of an int: %i byte(s)\n",sizeof(i));
-	printf("Size of a long int: %i byte(s)\n",(long int)sizeof(i));
-	printf("Size of a long long int: %i byte(s)\n",(long long int)sizeof(i));
-	printf("Size of a float: %i byte(s)\n",sizeof(f));
+
+	printf("Size of a char: %i byte(s)\n", sizeof(c));
+	printf("Size of an int: %i byte(s)\n", sizeof(i));
+	printf("Size of a long int: %i byte(s)\n", sizeof(j));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(k));
+	printf("Size of a float: %i byte(s)\n", sizeof(f));
 	return (0);
 }
