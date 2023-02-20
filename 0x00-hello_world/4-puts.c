2a79b4e1ae018474f3684221abb99ffcac6d8fe4
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-02-20 11:18:00 -0800

    last last correciton of 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints  "Programming is like building a multilingual puzzle to the std IO
+ * main - prints  "Programming is like building a multilingual puzzle to the st
+ *-d IO
  *
  * Return: 0 when code successful
  */
