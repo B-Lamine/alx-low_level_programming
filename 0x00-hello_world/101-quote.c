e41e8f13fc4c5cfede6c69155f14c39b8bb30722
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-02-21 10:47:17 -0800

    last correction of 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
 
 /**
- * main - returns stderr to stdo
+ * main - prints str to  stderr
  *
  * Return: 1
  */
 int main(void)
 {
-	system("echo $(hello world!) >2 2&>1");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n\
+", 61);
 	return (1);
 }
