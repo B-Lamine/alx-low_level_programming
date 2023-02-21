55837782f135dec25f19083aa5594d9a4af5175f
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-02-21 12:59:09 -0800

    correction 21.59 of task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,16 +2,14 @@
 #include "main.h"
 
 /**
- *
  * main - prints str "putchar" to stdout.
  *
  * Return: 0 when code successful.
  */
-
 int main(void)
 {
 	unsigned int i;
-	char cc[10] = {'_','p','u','t','c','h','a','r','\n'};
+	char cc[10] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\n'};
 
 	for (i = 0; i < strlen(cc); i++)
 		_putchar(cc[i]);
