e428f7a5ce371194d24aebba95815e408ee5dcf1
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-02-22 07:32:28 -0800

    initial commit for task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers.
+ * @a: integer to add.
+ * @b: integer to add.
+ *
+ * Return: result of @a + @b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
