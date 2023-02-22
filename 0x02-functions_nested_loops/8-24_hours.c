53ec9d49ff2c9a51b6ee0a2fbb6520230bc8ee4f
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-02-22 06:13:36 -0800

    initial commit for task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * jack_bauer - displays hour count in format 00:00.
+ */
+void jack_bauer(void)
+{
+	int h2 = 0;
+
+	while (h2 < 10)
+	{
+		int h1 = 0;
+
+		while (h1 < 10)
+		{
+			int m2 = 0;
+
+			while (m2 < 6)
+			{
+				int m1 = 0;
+
+				while (m1 < 10)
+				{
+					_putchar(h2 + '0');
+					_putchar(h1 + '0');
+					_putchar(':');
+					_putchar(m2 + '0');
+					_putchar(m1 + '0');
+					_putchar('\n');
+					m1++;
+				}
+				m2++;
+			}
+			if (h2 == 2 && h1 == 3)
+				return;
+			h1++;
+		}
+		h2++;
+	}
+}
