b3f53236effcd77c96205a6fefe80767312a042d
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-02-23 06:01:04 -0800

    initial commit for task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers.
+ * @a: factor.
+ * @b: factor.
+ *
+ * Return: return result of multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
