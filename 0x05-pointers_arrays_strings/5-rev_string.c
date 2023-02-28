2cb15dfb1380a6cef943934d2e1edf4613ea1709
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-02-28 03:19:29 -0800

    initial commit for task 7

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 int _strlen(char *s);
 
 /**
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "main.h"
+int _strlen(char *s);
+
+/**
+  * _strlen - counts length of the string.
+  * @s: string.
+  *
+  * Return: length of the string.
+  */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+		i++;
+	return (i);
+}
+
+/**
+  * puts_half - prints the last half of a string,
+  * excluding the middle character if length of the string is odd.
+  * @str: address of the string.
+  */
+void puts_half(char *str)
+{
+	int i, length_of_the_string;
+
+	length_of_the_string = _strlen(str);
+	if (length_of_the_string % 2 == 0)
+		i = length_of_the_string / 2;
+	else
+		i = (length_of_the_string - 1) / 2;
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
