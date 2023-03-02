46cbf58c9abc56f9522c785aea3b2d5c17a9a3d6
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-03-02 14:20:00 -0800

    3-strcmp.c: removing unused var. declarations

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,22 +1,6 @@
 #include "main.h"
 
 /**
-  * _strlen - counts length of the string.
-  * @s: string.
-  *
-  * Return: length of the string.
-  */
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while (*(s + i) != '\0')
-		i++;
-	return (i);
-}
-
-/**
   * _strcmp - compares two strings.
   * @s1: first string to be compared.
   * @s2: second string to be compared.
@@ -25,12 +9,10 @@ int _strlen(char *s)
   */
 int _strcmp(char *s1, char *s2)
 {
-	int i, diff, len1, len2;
+	int i, diff;
 
 	i = 0;
 	diff = 0;
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
 	while (*(s1 + i) != '\0' || *(s2 + i) != '\0')
 	{
 		diff = *(s1 + i) - *(s2 + i);
