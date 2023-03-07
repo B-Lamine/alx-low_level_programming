6b87308494d6bafb1db2906b3eadfe406088bb31
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-03-07 12:38:13 -0800

    3-strspn.c: returns length before first occurence.

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,7 +17,8 @@ int _strlen(char *s)
 }
 
 /**
-  * _strspn - counts the number of characters in @s that are also in @accept.
+  * _strspn - counts the number of characters in @s before first occurence of
+  * character in @accept.
   * @s: pointer to initial string.
   * @accept: pointer to string of accepted characters.
   *
@@ -26,25 +27,23 @@ int _strlen(char *s)
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, len1, len2;
-	unsigned int n;
 
 	len1 = _strlen(s);
 	len2 = _strlen(accept);
 	i = 0;
-	n = 0;
-	while (i < len1)
+	while (i < len2)
 	{
 		j = 0;
-		while (j < len2)
+		while (j < len1)
 		{
-			if (*(s + i) == *(accept + j))
+			if (*(s + j) == *(accept + i))
 			{
-				n++;
-				break;
+				j++;
+				return (j);
 			}
 			j++;
 		}
 		i++;
 	}
-	return (n);
+	return (0);
 }
