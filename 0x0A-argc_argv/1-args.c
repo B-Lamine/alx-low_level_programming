88b2b822e6837b5687d340494d28ab89b16f013d
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-03-10 12:43:49 -0800

    initial commit for 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - prints number of arguments passed to the program.
+  * @argc: number of arguments.
+  * @argv: array of arguments.
+  *
+  * Return: always 0.
+  */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,4 @@
 Here are code used to practice pasing arguments to the main program (argc and argv).
 
 0-whatsmyname : prints the name of the program.
+1-args.c : prints the number of arguments passed to the program through the command line.
