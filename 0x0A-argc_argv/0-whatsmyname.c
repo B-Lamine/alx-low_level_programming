addfecd3b6fb181d01b7a3f2c89ad58e2510199e
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-03-10 11:59:39 -0800

    0-whatsmyname.c: declaring argc unused

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
   * @arv: an array of arguments passed to the program.
   * Return: 0 if code successful.
   */
-int main(int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
