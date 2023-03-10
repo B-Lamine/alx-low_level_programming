413f6dfc05f83c0b7c2a8148c110d6f4c5c1fadb
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-03-10 13:08:57 -0800

    initial commit for 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - prints the product of two integers.
+  * @argc: number of arguments.
+  * @argv: arguments.
+  *
+  * Return: 0 if code successful, or 1 if no arguments are passed.
+  */
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc == 1)
+	{
+		printf("Error \n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	printf("%d\n", (a * b));
+	return (0);
+}
