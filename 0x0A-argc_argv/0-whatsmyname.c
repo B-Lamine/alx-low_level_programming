9abe36224b795bb0c28a322c89033ac0840ec468
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-03-10 11:55:15 -0800

    initial commit for 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - prints name of the executable file.
+  * @argc: number of arguments passed to the program.
+  * @arv: an array of arguments passed to the program.
+  * Return: 0 if code successful.
+  */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+Here are code used to practice pasing arguments to the main program (argc and argv).
+
+0-whatsmyname : prints the name of the program.
