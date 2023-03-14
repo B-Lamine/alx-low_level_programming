1597551a5853b7d0a2e3096c49d92f91e96f0b2c
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-03-14 09:46:38 -0700

    initial commit for 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * create_array - creates an array of given size, and initializes with
+  * a given character.
+  * @size: size of the array.
+  * @c: initialization character.
+  *
+  * Return: the address to the array if successful, or NULL if @size = 0 or
+  * memory allocation fails.
+  */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ar;
+
+	i = 0;
+	ar = malloc(sizeof(char) * size);
+	while (i < size)
+	{
+		*(ar + i) = c;
+		i++;
+	}
+	if (ar == NULL)
+		return (0);
+	else
+		return (ar);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+This contains code for practicing dynamic memory allocation using malloc() and free() functions.
+
+0-create_array.c : creates an array of given size and initializes it with a given character.
