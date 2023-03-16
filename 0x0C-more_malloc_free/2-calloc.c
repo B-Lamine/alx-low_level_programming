68c29e5a4fe9a6257b863391b390cbcc06da4374
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-03-16 08:40:57 -0700

    initial commit for task 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * _calloc - allocates memory for an array of @nmemb elements of @size size.
+  * @nmemb: number of elements.
+  * @size: size in bytes of each element.
+  *
+  * Return: address to the array.
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	return (ptr);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,3 +1,5 @@
 Contains code for further practice of dynamic memory allocation using malloc(), free(), calloc(), realloc() but also exit() for normal process termination.
 
 0-malloc_checked.c : allocates memory of a given size, no matter the type. If the allocation fail, the program terminates with an exit value of 98.
+1-string_nconcat.c : concatenates two strings.
+2-calloc.c : allocates memory for an array of given number of elements each of given size.
