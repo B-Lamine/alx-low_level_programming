66b1e24847ce45462643eca92a913c93ada30813
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-03-16 07:05:50 -0700

    1-string_nconcat.c: return NULL if both strings are NULL

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,6 +12,8 @@ int _strlen(char *s)
 	int i;
 
 	i = 0;
+	if (s == NULL)
+		return (0);
 	while (*(s + i) != '\0')
 		i++;
 	return (i);
@@ -31,6 +33,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, len1, len2;
 	char *ptr;
 
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	ptr = malloc(sizeof(*s1) * len1 + n + 1);
