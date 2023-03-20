faf4623473491c30c32049e6150b9aced74e1f59
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-03-20 15:10:07 -0700

    dog.h: initial commit

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+Includes code to practice structs and typedef usage.
+
+dog.h : defines a struct dog, for the dog's name, age and its owner's name.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG
+#define DOG
+
+/**
+  * struct dog - defines a dog's name, age and owner's name.
+  * @name: dog's name,
+  * @age: dog's age,
+  * @owner: dog owner's name.
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* #ifndef DOG */
