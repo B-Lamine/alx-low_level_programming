bdf57c3a56b57968a46e53666647d2edc1db899d
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-03-20 16:08:14 -0700

    1-init_dog.c: initial commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <stdio.h>
+
+/**
+  * init_dog - initializes a struct dog by the given arguments.
+  * @d: address of the struct,
+  * @name: dog's name,
+  * @age : dog's age,
+  * @owner: owner's name.
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,3 +1,4 @@
 Includes code to practice structs and typedef usage.
 
 dog.h : defines a struct dog, for the dog's name, age and its owner's name.
+1-init_dog.c : initializes a struct dog, by the given input.
