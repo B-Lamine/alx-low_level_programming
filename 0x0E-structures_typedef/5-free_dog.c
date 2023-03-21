1e366a0e3bdda6c6b820b4320518fdeb41f9d43d
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-03-21 03:23:06 -0700

    5-free_dog.c: initial commit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+  * free_dog - free allocated memory to given dog.
+  * @d: pointer to dog struct.
+  */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d);
+}
