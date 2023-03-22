a3ae77c0bab8f88e5036cbc3dcfa8b63dd07d6b4
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-03-22 09:35:06 -0700

    2-int_index.c: initial commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+  * int_index - searches a given array for an integer using function cmp.
+  * @array: array of integers,
+  * @size: array's size,
+  * @cmp: function for identifying integer.
+  * Return: the index of the desired integer in the array if found, -1 if not.
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	i = 0;
+	while (i < size)
+	{
+		if (cmp[i] != 0)
+			return (-1);
+		i++;
+	}
+	return (-1);
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -2,3 +2,4 @@ This contaiins code for practicing pointers to functions.
 
 0-print_name.c : inputs given string to given function.
 1-array_iterator.c : performs a given action on all elements of a given array.
+2-int_index.c : looks for a integer in a given array.
