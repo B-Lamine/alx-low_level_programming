e1c19f04a5b62849fd928b5f613344f53bb74da5
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-03-22 09:38:53 -0700

    2-int_index.c: return index if integer is found.

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,8 +16,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	i = 0;
 	while (i < size)
 	{
-		if (cmp[i] != 0)
-			return (-1);
+		if (cmp(array[i]) != 0)
+			return (i);
 		i++;
 	}
 	return (-1);
