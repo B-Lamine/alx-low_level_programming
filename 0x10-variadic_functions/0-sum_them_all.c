a65fa25bedc9e6713cc9199fa0f3a182d59e92dd
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-03-23 04:59:56 -0700

    0-sum_them_all.c: initial commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+  * sum_them_all - sums all of any number of integer input.
+  * @n: number of arguments passed.
+  * Return: the sum of the arguments, or 0 if none are passed.
+  */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int sum;
+	unsigned int i;
+
+	if (n ==0)
+		return (0);
+	va_start(ap, n);
+	sum = 0;
+	i = 0;
+	while (i < n)
+	{
+		sum = sum + va_arg(ap, int);
+		i++;
+	}
+	va_end(ap);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,3 @@
+This contains code for practicing variadic (orargs) functions.
+
+0-sum_them_all.c : sums all of any given number of integer input.
