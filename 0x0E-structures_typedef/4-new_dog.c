40e90215578f9d05de73d3d7a870e975ca8da29a
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-03-29 06:49:58 -0700

    4-new_dog.c: free all ptrs in case malloc fails

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,43 @@
 #include <stdlib.h>
 
 /**
+  * _strlen - counts length of the string.
+  * @s: string.
+  *
+  * Return: length of the string.
+  */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+		i++;
+	return (i);
+}
+
+/**
+  * _strcpy - takes a string and copies it to a buffer.
+  * @dest: buffer address.
+  * @src: string to be copied.
+  *
+  * Return: address to the buffer.
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int i, len;
+
+	i = 0;
+	len = _strlen(src);
+	while (i <= len)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
+
+/**
   * new_dog - creates a new dog.
   * @name: new dog's name,
   * @age: new dog's age,
@@ -11,12 +48,31 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newd;
+	char *nme, *onr;
+	float a;
+	int name_len, owner_len;
 
+	a = age;
 	newd = malloc(sizeof(dog_t));
 	if (newd == NULL)
 		return (NULL);
-	newd->name = name;
-	newd->age = age;
-	newd->owner = owner;
+	name_len = _strlen(name);
+	nme = malloc(sizeof(*nme) * (name_len + 1));
+	if (nme == NULL)
+	{
+		free(newd);
+		return (NULL);
+	}
+	owner_len = _strlen(owner);
+	onr = malloc(sizeof(*onr) * (owner_len + 1));
+	if (owner == NULL)
+	{
+		free(nme);
+		free(newd);
+		return (NULL);
+	}
+	newd->name = _strcpy(nme, name);
+	newd->age = a;
+	newd->owner = _strcpy(onr, owner);
 	return (newd);
 }
