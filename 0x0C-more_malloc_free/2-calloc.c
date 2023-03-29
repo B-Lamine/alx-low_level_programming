9a1cae805f5da87a2c916c0cd565dacf6a71225c
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-03-29 07:11:36 -0700

    2-calloc.c: using _memset to set array to 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,6 +2,28 @@
 #include <stdlib.h>
 
 /**
+  * _memset - fills with character @b the @n first bytes of the memory pointer
+  * to by @s.
+  * @s: pointer to memory area.
+  * @b: byte to set memory to.
+  * @n: number of bytes to be set.
+  *
+  * Return: pointer to the memory area.
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
+}
+
+/**
   * _calloc - allocates memory for an array of @nmemb elements of @size size.
   * @nmemb: number of elements.
   * @size: size in bytes of each element.
@@ -16,11 +38,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
-	{
-		free(ptr);
 		return (ptr);
-	}
-	*(char *)ptr = 0;
+	ptr = (void *)_memset(ptr, 0, size);
 	return (ptr);
 }
 
