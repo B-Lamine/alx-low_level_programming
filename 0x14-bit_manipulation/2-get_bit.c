644ae76e19c85024dbfeb27174f8bd9b01ada396
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-04-13 01:59:12 -0700

    2-get_bit.c: initial commit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * get_bit - gets value of bit at given index of the given number.
+  * @n: given number,
+  * @index: position of the bit in the binary form of @n.
+  * Return: bit at @index.
+  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask = 1;
+	unsigned int check;
+
+	mask = mask << index;
+	check = ((n & mask) && mask);
+	if (check != 0 || check != 1)
+		return (check);
+	else
+		return (-1);
+}
