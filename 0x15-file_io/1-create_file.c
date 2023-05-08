52c9c142625f6a74865f38b1a6d0bac7929d5f85
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-05-08 14:28:07 -0700

    1-create_file: initial commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen - counts the number of characters in a string.
+ * @s: string address.
+ * Return: length of the string, terminating byte excluded.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * create_file - creates @filename file, or overwrites existing one.
+ * @filename: address of the file.
+ * @text_content: string to write to file.
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len, bytes_check;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		len = _strlen(text_content);
+		bytes_check = write(fd, text_content, len);
+		if (bytes_check != len)
+			return (-1);
+	}
+	close(fd);
+	return (-1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,8 @@
 #define FIO
 #include <stddef.h>
 #include <unistd.h>
+#include <fcntl.h>
 #define BUFSIZE 1024
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif /* #ifndef FIO */
