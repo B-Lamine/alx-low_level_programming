31b8a9d99da59b8759fd11a637c104c202b84a2a
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-06-15 05:07:25 -0700

    4-free_dlist_int: initial commit

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+  * free_dlistint - free doubly linked list.
+  * @head: pointer to first node.
+  */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *cursor;
+
+	if (head == NULL)
+		return;
+	cursor = head;
+	while (cursor->next->next != NULL)
+		cursor = cursor->next;
+	while (cursor != NULL)
+	{
+		free(cursor->next);
+		cursor = cursor->prev;
+	}
+	free(head);
+}
