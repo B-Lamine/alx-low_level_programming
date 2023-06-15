9f0bff8ecf14de8680e170f574a1bff4ef01be2a
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-06-15 03:42:36 -0700

    add_nodeint_end: initial commit

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+  * add_dnodeint_end - add node at end of doubly linked list.
+  * @head: address of pointer to the first node.
+  * @n: integer to store in the new node.
+  * Return: address of new node.
+  */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *tmp, *new;
+
+	new = malloc(sizeof(*new));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (*head == NULL)
+	{
+		*head = new;
+		new->next = NULL;
+		new->prev = NULL;
+		return (new);
+	}
+	tmp = *head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = new;
+	new->next = NULL;
+	new->prev = tmp;
+	return (new);
+}
