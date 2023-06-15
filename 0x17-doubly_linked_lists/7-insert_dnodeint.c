d5e9b8d8095002507bda4536fd599a1774d4ed57
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-06-15 07:20:01 -0700

    insert_dnodeint_at_index: conform to betty style and documentation

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 
 /**
+  * insert_dnodeint_at_index - add node at a specific position in the doubly
+  * linked list.
+  * @h: adress of pointer to first node.
+  * @idx: index of node to insert.
+  * @n: integer to store in new node.
+  * Return: address of new node.
   */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
@@ -19,5 +25,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	tmp = cursor->prev;
 	tmp->next = add_dnodeint(&cursor, (const int)n);
-	return(tmp->next);
+	return (tmp->next);
 }
