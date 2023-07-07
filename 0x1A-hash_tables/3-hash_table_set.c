f333f09f2c40faef459310d5f4b29ffd38692ad1
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-07-07 12:47:24 -0700

    3-hash_table_set: set new_nde as head if element is empty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *ht_node, *cursor, *last_node, *new_node;
+	hash_node_t *cursor, *last_node, *new_node;
 
 	if (ht == NULL || key == NULL)
 		return (0);
@@ -23,24 +23,28 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->value = strdup(value);
 	new_node->next = NULL;
 	index = key_index((unsigned char *)key, ht->size);
-	ht_node = ht->array[index];
-	if (ht_node == NULL)
+	if (ht->array[index] == NULL)
 	{
-		ht_node = new_node;
+		ht->array[index] = new_node;
+		printf("key:%s\tvalue:%s\n", ht->array[index]->key, ht->array[index]->value);
 		return (1);
 	}
+	last_node = ht->array[index];
 	cursor = ht->array[index];
 	while (cursor != NULL)
 	{
+		printf("key:%s\tvalue:%s\n", cursor->key, cursor->value);
 		if (strcmp(cursor->key, key) == 0)
 		{
 			cursor->key = strdup(key);
 			cursor->value = strdup(value);
+			printf("key:%s\tvalue:%s\n", cursor->key, cursor->value);
 			return (1);
 		}
 		last_node = cursor;
 		cursor = cursor->next;
 	}
 	last_node->next = new_node;
+	printf("key:%s\tvalue:%s\n", last_node->key, last_node->value);
 	return (1);
 }
